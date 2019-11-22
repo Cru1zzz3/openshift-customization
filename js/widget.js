1064e7acdcc4c79dfc47795961a2224de9696a7b
Stanislav Udartsev <udartzev_sv@mail.ru>
2019-11-22 22:12:27 +0300

    organize code

--- js/widget.js
+++ js/widget.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 angular
   .module('openshiftOnlineExtensions', ['openshiftConsole'])
   .run([
@@ -9,8 +7,7 @@ angular
     function (extensionRegistry) {
 
       var system_status_elem = $('<a href="https://cru1zzz3.github.io/openshift-customization/json/data.json" target="_blank" class="nav-item-iconic system-status project-action-btn">');
-      //var system_status_elem_mobile = $('<div row flex class="navbar-flex-btn system-status-mobile">');
-
+      
       $.getJSON("https://cru1zzz3.github.io/openshift-customization/json/data.json", function (data) {
         var featureCount = Object.keys(data).length;
         var items = [];
@@ -25,26 +22,11 @@ angular
         $('<span title="System Status" class="fa status-icon pficon-warning-triangle-o"></span>').appendTo(system_status_elem);
         $('<span class="status-issue">' + featureStr + '</span>').appendTo(system_status_elem);
 
+        var widget_dropdown = $('<a id="widget-dropdown" class="nav-item-iconic dropdown-toggle" uib-dropdown-toggle=""><span class="sr-only">Help</span></a>')
+
+        var feature_status = $('<span class="feature-status">' + Object.keys(data) + ':' + Object.values(data) + '</span>');
 
         extensionRegistry
-          .add('nav-system-status-mobile', function () {
-            return [{
-              type: 'dom',
-              node: system_status_elem_mobile
-            }];
-          });
-
-        //system_status_elem_mobile.append(system_status_elem.clone());
-
-        // only add the extension if there is something to show so we
-        // do not generate empty nodes if no issues
-
-      });
-      var widget_dropdown = $('<a id="widget-dropdown" class="nav-item-iconic dropdown-toggle" uib-dropdown-toggle=""><span class="sr-only">Help</span></a>')
-
-      var feature_status = $('<span class="feature-status">' + Object.keys(data) + ':' + Object.values(data) + '</span>');
-
-      extensionRegistry
         .add('nav-system-status', function () {
           return [{
             type: 'dom',
@@ -57,62 +39,8 @@ angular
             node: feature_status
           }];
         });
-
+      });
     }
   ]);
 
 pluginLoader.addModule('openshiftOnlineExtensions');
-
-/*
-angular
-  .module('mysystemstatusbadgeextension', ['openshiftConsole'])
-  .run([
-    'extensionRegistry',
-    function (extensionRegistry) {
-      // Replace http://status.example.com/ with your domain
-      var system_status_elem = $('<a href="http://status.example.com/"' +
-        'target="_blank" class="nav-item-iconic system-status"><span title="' +
-        'System Status" class="fa status-icon pficon-warning-triangle-o">' +
-        '</span></a>');
-
-      // Add the extension point to the registry so the badge appears
-      // To disable the badge, comment this block out
-      extensionRegistry
-        .add('nav-system-status', function () {
-          return [{
-            type: 'dom',
-            node: system_status_elem
-          }];
-        });
-    }
-  ]);
-
-hawtioPluginLoader.addModule('mysystemstatusbadgeextension');
-*/
-
-/*
-angular
-  .module('openshiftMonitoringWidget', ['openshiftConsole'])
-  .run([
-    'extensionRegistry',
-    function(extensionRegistry) {
-      extensionRegistry
-        .add('nav-help-dropdown', function() {
-          return [
-            {
-              type: 'dom',
-              node: '<li><a href="http://www.example.com/report" target="_blank">Report a Bug</a></li>'
-            }, {
-              type: 'dom',
-              node: '<li class="divider"></li>'  // If you want a horizontal divider to appear in the menu
-            }, {
-              type: 'dom',
-              node: '<li><a href="http://www.example.com/status" target="_blank">System Status</a></li>'
-            }
-          ];
-        });
-    }
-  ]);
-
-pluginLoader.addModule('openshiftMonitoringWidget');
-*/
