a34ea0f21322010843e370595e49bfdd8c1d70f3
Stanislav Udartsev <udartzev_sv@mail.ru>
2019-11-23 02:38:07 +0300

    cosmetic organize code

--- js/widget.js
+++ js/widget.js
@@ -1,59 +1,61 @@
 'use strict';
 
 angular
-  .module('openshiftOnlineExtensions', ['openshiftConsole'])
+  .module('openshiftMonitoringWidget', ['openshiftConsole'])
   .run([
     'extensionRegistry',
     function (extensionRegistry) {
       $.getJSON("https://cru1zzz3.github.io/openshift-customization/json/data.json", function (data) {
         var featureCount = Object.keys(data).length; // amount of features
                 
-
-        function feature_status() {
+        // print all features from JSON file
+        function feature_status() { 
           var features = "";
           $.each(data, function (key, val) {
             features = features.concat('<li class="ng-scope">' + key + ' : ' + val + '</li>');
           });
           return features;   
         }
-
-        var featureStr = featureCount + ' feature';
-        if (featureCount !== 1) {
-          featureStr += "s";
-        }
-
+        
         extensionRegistry
           .add('nav-widget-dropdown', function () {
             return [{
               type: 'dom',
-              node: feature_status()
+              node: feature_status() 
             }];
           });
 
-        var system_status_elem = $('<li class="dropdown" uib-dropdown="" style="padding-top: 20px">' +
-          '<a id="widget-dropdown" uib-dropdown-toggle="" class="nav-item-iconic dropdown-toggle" href="" aria-haspopup="true" aria-expanded="false">' +
-            '<span title="System Status" class="fa object fa-tachometer"></span>' + // dashboard icon
-            '<span>'+ featureStr + '</span>' +                                      // print amount of features
-            '<span class="sr-only">Status dashboard</span>' +                       // hover comment
-            '<span class="caret" aria-hidden="true"></span>' +                      // arrow icon
+        var featureStr = ' ' + featureCount + ' feature';
+        if (featureCount !== 1) {
+          featureStr += "s";
+        }
+
+        var status_widget = $('<li class="dropdown" uib-dropdown="" style="padding-top: 20px">' +   // dropdown element on page
+          '<a id="widget-dropdown" uib-dropdown-toggle="" class="nav-item-iconic dropdown-toggle"' +     
+            'href="" aria-haspopup="true" aria-expanded="false">' +
+              '<span title="System Status" class="fa object fa-tachometer"></span>' +                     // dashboard icon
+              '<span>'+ featureStr + '</span>' +                                                          // print amount of features
+              '<span class="sr-only">Status dashboard</span>' +                                           // hover comment
+              '<span class="caret" aria-hidden="true"></span>' +                                          // arrow icon
           '</a>' +                                                                  
             '<ul uib-dropdown-menu="" aria-labelledby="widget-dropdown" extension-point="" extension-name="nav-widget-dropdown"' +
-              'extension-types="dom html" class="ng-isolate-scope dropdown-menu">' + // dropdown menu
-                          
+              'extension-types="dom html" class="ng-isolate-scope dropdown-menu">' +                      // dropdown menu
+              // here will be input of "nav-widget-dropdown" extension           
             '</ul>' +
           '</li>'
         );
-
+        
+        // extension add widget to
         extensionRegistry
           .add('nav-system-status', function () {
             return [{
               type: 'dom',
-              node: system_status_elem
+              node: status_widget
             }];
           });
       });
     }
   ]);
 
-pluginLoader.addModule('openshiftOnlineExtensions');
+pluginLoader.addModule('openshiftMonitoringWidget');
 
