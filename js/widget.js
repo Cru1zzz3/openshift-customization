fcacfabbf3dfbd2a4cc1dc0ae0b81ed0abda1e5d
Stanislav Udartsev <udartzev_sv@mail.ru>
2019-12-09 23:36:50 +0300

    add getMetrics to node in nav-widget-dropdown

--- js/widget.js
+++ js/widget.js
@@ -24,6 +24,9 @@ angular
             return [{
               type: 'dom',
               node: feature_status()
+            },{
+              type: 'dom',
+              node: getMetics()
             }];
           });
 
@@ -32,7 +35,7 @@ angular
           featureStr += "s";
         }
 
-        function getMetics(){
+        metrics = function getMetics() {
           var xhr = new XMLHttpRequest();
           xhr.open('GET', '/metrics')
           xhr.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJvcGVuc2hpZnQtbW9uaXRvcmluZyIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJjbHVzdGVyLW1vbml0b3Jpbmctb3BlcmF0b3ItdG9rZW4taDhod2IiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiY2x1c3Rlci1tb25pdG9yaW5nLW9wZXJhdG9yIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiMjJhMGQyZTAtMTljNy0xMWVhLTkyYzEtMDIwMDE3MDBiZGIxIiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50Om9wZW5zaGlmdC1tb25pdG9yaW5nOmNsdXN0ZXItbW9uaXRvcmluZy1vcGVyYXRvciJ9.WaOw4RShMJDlcDV7NUJCcp2Ba7Lvm7CKxywGijR2pZqcyI70NUeLE9Vx3pqUznPbL1biGRPdRllfEStVQq9QywCg16zi2Q9Ka0zIyg4vlt9seWuwctPEEdQitXqPnrlwuyzqcNO0tnS2AFjoeSxhOIlocLcfW-WvBARyEYr7kjKnH4mwvBCK3bklIh5hsub6LLVLqBSqx72q4fjsHQz7fBrlcRFj_qTSrIDoSBfYDkSGHojCQ48DbWHzm0KmaGspEFnT06QNogm1FwKRq08dZ0oaDf1WFIVZ_LTqYarSyDTU3aQ6TW_VNd8nEOLXmq3PQ8p1U5lEAhYPq7qvvt0tCg')
@@ -51,12 +54,14 @@ angular
           }
         }
 
-        getMetics();
+        //getMetics();
+
+        //var refreshMetricsScript = $('<script>' + metrics + '</script>')
 
         var status_widget = $('<li class="dropdown" uib-dropdown="" style="padding-top: 20px">' +         // dropdown element on page
           '<a id="widget-dropdown" uib-dropdown-toggle="" class="nav-item-iconic dropdown-toggle"' +
-          'href="" aria-haspopup="true" aria-expanded="false">' + 
-          '<button id="refreshMetrics" type="button" onclick="getMetics()" title="Refresh metrics" class="fa action fa-refresh"> </button>'+
+          'href="" aria-haspopup="true" aria-expanded="false">' +
+          '<button id="refreshMetrics" type="button" onclick="getMetics()" title="Refresh metrics" class="fa action fa-refresh"> </button>' +
           '<span title="System Status" class="fa object fa-tachometer"></span>' +                     // dashboard icon
           '<span>' + featureStr + '</span>' +                                                          // print amount of features
           '<span class="sr-only">Status dashboard</span>' +                                           // hover comment
@@ -78,8 +83,8 @@ angular
             }];
           });
 
-        
-       
+
+
         /*
         var response = [];
         $.ajax({
