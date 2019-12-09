1d2ee5f6965657b9d6eb0471f93107d38005e5c7
Stanislav Udartsev <udartzev_sv@mail.ru>
2019-12-09 23:44:02 +0300

    separate refresh button from dropdown-menu

--- js/widget.js
+++ js/widget.js
@@ -58,10 +58,11 @@ angular
 
         //var refreshMetricsScript = $('<script>' + metrics + '</script>')
 
+        var refresh_button = $('<button id="refreshMetrics" type="button" onclick="getMetics()" title="Refresh metrics" class="fa action fa-refresh"> </button>');
+        
         var status_widget = $('<li class="dropdown" uib-dropdown="" style="padding-top: 20px">' +         // dropdown element on page
           '<a id="widget-dropdown" uib-dropdown-toggle="" class="nav-item-iconic dropdown-toggle"' +
           'href="" aria-haspopup="true" aria-expanded="false">' +
-          '<button id="refreshMetrics" type="button" onclick="getMetics()" title="Refresh metrics" class="fa action fa-refresh"> </button>' +
           '<span title="System Status" class="fa object fa-tachometer"></span>' +                     // dashboard icon
           '<span>' + featureStr + '</span>' +                                                          // print amount of features
           '<span class="sr-only">Status dashboard</span>' +                                           // hover comment
@@ -79,29 +80,12 @@ angular
           .add('nav-system-status', function () {
             return [{
               type: 'dom',
+              node: refresh_button // add refresh button 
+            },{
+              type: 'dom',
               node: status_widget // maybe change to monitoring widget
             }];
           });
-
-
-
-        /*
-        var response = [];
-        $.ajax({
-          type: "GET", //GET, POST, PUT
-          url: '/metrics',  //the url to call
-          beforeSend: function (xhr) {   //Set token here
-            var token = 'yJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJvcGVuc2hpZnQtbW9uaXRvcmluZyIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJjbHVzdGVyLW1vbml0b3Jpbmctb3BlcmF0b3ItdG9rZW4taDhod2IiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiY2x1c3Rlci1tb25pdG9yaW5nLW9wZXJhdG9yIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiMjJhMGQyZTAtMTljNy0xMWVhLTkyYzEtMDIwMDE3MDBiZGIxIiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50Om9wZW5zaGlmdC1tb25pdG9yaW5nOmNsdXN0ZXItbW9uaXRvcmluZy1vcGVyYXRvciJ9.WaOw4RShMJDlcDV7NUJCcp2Ba7Lvm7CKxywGijR2pZqcyI70NUeLE9Vx3pqUznPbL1biGRPdRllfEStVQq9QywCg16zi2Q9Ka0zIyg4vlt9seWuwctPEEdQitXqPnrlwuyzqcNO0tnS2AFjoeSxhOIlocLcfW-WvBARyEYr7kjKnH4mwvBCK3bklIh5hsub6LLVLqBSqx72q4fjsHQz7fBrlcRFj_qTSrIDoSBfYDkSGHojCQ48DbWHzm0KmaGspEFnT06QNogm1FwKRq08dZ0oaDf1WFIVZ_LTqYarSyDTU3aQ6TW_VNd8nEOLXmq3PQ8p1U5lEAhYPq7qvvt0tCg'
-            xhr.setRequestHeader("Authorization", 'Bearer ' + token);
-          }
-        }).done(function (response) {
-          console.log("Authorization ok");
-          plainelement = document.getElementById("plain");
-          plainelement.append(xhr.responseText)
-        }).fail(function () {
-          alert("Error in getting metrics. Reason" + response.responseType + response.responseText);
-        });
-        */
       });
     }
   ]);
